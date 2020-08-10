9f02d40578106207a039e717f2a84b81ad4310be
hanamhn <hanamhndev@gmail.com>
2020-08-10 14:08:27 +0700

    add image

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import footerStyles from './footer.module.scss'
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -12,8 +13,8 @@ const Footer = () => {
     }
   `)
   return (
-    <footer>
-      <p>Created by {data.site.siteMetadata.author}, © 2019</p>
+    <footer className={footerStyles.footer}>
+      <p>Created by {data.site.siteMetadata.author}, © 2020</p>
     </footer>
   )
 }
--- src/posts/gatsby/gatsby.md
+++ src/posts/gatsby/gatsby.md
@@ -5,6 +5,8 @@ date: "2020-08-07"
 
 I just launched a new bootcamp!
 
+![Grass](./grass.png)
+
 ## Topics Covered
 
 1. Gatsby
